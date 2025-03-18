c0d8e6473be87584bacb44ffe591957406a5e67c
Mishal-A <mishal45654@gmail.com>
2025-03-18 22:43:29 +0500

    Delete SCRUM-5-Event-Service-REST-API-integration directory

--- SCRUM-5-Event-Service-REST-API-integration/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controllers = require('./controllers');
-
-router.post('/', controllers.event_creator);
-router.get('/', controllers.find_all_events);
-router.get('/:event_id/availability', controllers.find_if_event_avail);
-
-module.exports = router;
